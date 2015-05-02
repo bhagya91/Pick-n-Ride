93b9be61fc1f7acd525e68f79fc60c2b34d13f30
lpsandaruwan <lpsandaruwan@gmail.com>
2015-05-02 17:44:21 +0530

    Server initialized

---
+++ WebApp/src/com/picknride/WebApp/Server.java
@@ -0,0 +1,10 @@
+package com.picknride.WebApp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath( "picknride" )
+public class Server extends Application {
+	
+}
