a5ca6c8bccb64d68805692f9370d2e6190139064
lpsandaruwan <lpsandaruwan@gmail.com>
2015-05-04 22:09:02 +0530

    Server works with MongoDb test

--- WebApp/src/com/picknride/WebApp/Database.java
+++ WebApp/src/com/picknride/WebApp/Database.java
@@ -1,21 +1,36 @@
 package com.picknride.WebApp;
 
-import com.mongodb.Mongo;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCursor;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 @Path("db")
 public class Database extends Application{
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public String checkMon() throws Exception{
-		return "db";
+		MongoClient mongo = new MongoClient("localhost", 27017);
+		DB picknride = mongo.getDB("picknride");
+		DBCollection table = picknride.getCollection("passengers");
 		
+		BasicDBObject search = new BasicDBObject();
+		search.put("_id", "222");
+		
+		List<DBObject> cursor = table.find(search).toArray();
+		String myList= null;
+		myList = cursor.toString();
+		
+		return myList;
 	}
 }
